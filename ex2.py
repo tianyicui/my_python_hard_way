ca72790347df864d7e97e46260a051f2f04886aa
Tianyi Cui <tianyicui@gmail.com>
2010-10-26 19:15:35 +0800

    ex2 fix

--- ex2.py
+++ ex2.py
@@ -1,7 +1,7 @@
 # A comment, this is so you can read you program later.
 # Anything after the # is ignored by python.
 
-print "I could have code like this." # and the comment are ignored
+print "I could have code like this." # and the comment after are ignored
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # print "This won't run."
