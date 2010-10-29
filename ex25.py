be8a657315fddce717ea89dca9abcf3063902485
Tianyi Cui <tianyicui@gmail.com>
2010-10-29 10:18:22 +0800

    ex25

---
+++ ex25.py
@@ -0,0 +1,80 @@
+"""
+This is the "ex25" module.
+
+Use `python ex25.py` to run doctests.
+Use `pydoc -w ./ex25.py` to generate HTML documentation.
+
+>>> import ex25
+>>> sentence = "All good things come to those who wait."
+>>> words = ex25.break_words(sentence)
+>>> words
+['All', 'good', 'things', 'come', 'to', 'those', 'who', 'wait.']
+>>> sorted_words = ex25.sort_words(words)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> ex25.print_first_word(words)
+All
+>>> ex25.print_last_word(words)
+wait.
+>>> wrods
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'wrods' is not defined
+>>> words
+['good', 'things', 'come', 'to', 'those', 'who']
+>>> ex25.print_first_word(sorted_words)
+All
+>>> ex25.print_last_word(sorted_words)
+who
+>>> sorted_words
+['come', 'good', 'things', 'those', 'to', 'wait.']
+>>> sorted_words = ex25.sort_sentence(sentence)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> ex25.print_first_and_last(sentence)
+All
+wait.
+>>> ex25.print_first_and_last_sorted(sentence)
+All
+who
+"""
+
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print word
+
+def print_last_word(words):
+    """Takes in a full sentence and returns the sroted words."""
+    word = words.pop(-1)
+    print word
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence);
+    print_first_word(words)
+    print_last_word(words)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
